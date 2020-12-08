b331956e2b6fd7abb9ff3ebc2f2ae9308e1b5a6f
vanct <vanct10a11999@gmail.com>
2020-12-09 05:33:46 +0700

    thuc hanh su dung toan tu

---
+++ thsudungtoantu.js
@@ -0,0 +1,12 @@
+let inputwidth;
+let inputheight;
+inputheight=prompt("Enter the height");
+inputwidth=prompt("Enter the width");
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width*height;
+document.write("Enter height: " + height);
+document.write('<br/>');
+document.write("Enter width: " + width);
+document.write('<br/>');
+document.write("The area is: " + are)
